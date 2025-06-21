27fe3347460b6874918b9315b78ba38b4c207232
hamma <kousirou0118@gmail.com>
2025-06-21 17:03:10 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import FileRenameApp from "@/components/FileRenameApp";
+// 相対パスに変更して確実に解決
+import FileRenameApp from "../components/FileRenameApp";
 
 export default function Home() {
   return (
